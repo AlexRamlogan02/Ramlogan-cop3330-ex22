d3e276f7388aa295545c7b9a950ca2e984f20b96
AlexRamlogan02 <90638917+AlexRamlogan02@users.noreply.github.com>
2021-09-13 23:44:01 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        int a, b, c;
+        System.out.print("Enter the first number : ");
+        a = scan.nextInt();
+        System.out.print("Enter the second number : ");
+        b = scan.nextInt();
+        System.out.print("Enter the third number : ");
+        c = scan.nextInt();
+        if ( a < b) {
+            if (b < c)
+                System.out.println(c + "Is the largest number");
+            else if (b > c)
+                System.out.println(b + "Is the largest number");
+        }
+        else if (a>c)
+        {
+            System.out.println(a +"Is the largest number");
+        }
+    }
+}
